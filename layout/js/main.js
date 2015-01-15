70ca0c741d2da10698811738299be1fbf0b840a2
inodesign21 <inodesign21@gmail.com>
2015-01-15 18:53:43 -0200

    arquivos iniciais

---
+++ layout/js/main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+
+	//Exibe placeholder na label
+	$('[placeholder]').stickyPlaceholders();
+
+	//Exibe dropdown ao passar o mouse
+	$('.dropdown-active').dropdownHover().dropdown();
+
+});
